37f4655e9548ff0a9e2494a4fc7789738dcb0389
Spencer Rohan <spencermrohan@gmail.com>
2017-01-18 22:30:39 -0600

    TEST SUITE WORKING & INITIAL STRUCTURE SETUP

---
+++ src/com/railwaysystem/java/Main.java
@@ -0,0 +1,10 @@
+package com.railwaysystem.java;
+
+/**
+ * Created by N/A on 1/18/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("START");
+    }
+}
