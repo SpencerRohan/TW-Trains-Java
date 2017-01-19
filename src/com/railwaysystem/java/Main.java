911912754435b48e84603a21f81ed2fd75bb0cce
Spencer Rohan <spencermrohan@gmail.com>
2017-01-19 03:23:16 -0600

    ADD psuedo code

--- src/com/railwaysystem/java/Main.java
+++ src/com/railwaysystem/java/Main.java
@@ -4,7 +4,16 @@ package com.railwaysystem.java;
  * Created by N/A on 1/18/17.
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("START");
+        RailwayGraph graph = (args.length == 1) ?
+                new RailwayGraph(args) : new RailwayGraph();
+
+
+        // Create a new graph controller
+        // Generate graph - with optional args if no string - check if graph.txt exists
+        // run railways system with returned graph data
+        System.out.println("Done");
     }
 }
