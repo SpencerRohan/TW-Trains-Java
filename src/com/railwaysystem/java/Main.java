bbbe763ff4a2d92ef7c2be9958e8de8a14ebc2ba
Spencer Rohan <spencermrohan@gmail.com>
2017-01-19 12:11:38 -0600

    UPDATE Main REMOVE previous models

--- src/com/railwaysystem/java/Main.java
+++ src/com/railwaysystem/java/Main.java
@@ -1,5 +1,9 @@
 package com.railwaysystem.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  * Created by N/A on 1/18/17.
  */
@@ -7,11 +11,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("START");
-        RailwayGraph graph = (args.length == 1) ?
-                new RailwayGraph(args) : new RailwayGraph();
+        String sourceFile = "assets/txt/sample-graph.txt";
+        String targetFile = "assets/txt/graph.txt";
+        String graphString;
+
+        if (args.length == 1){
+            graphString = args[0];
+        } else {
+            try {
+                FileReader fReader = new FileReader(sourceFile);
+                BufferedReader bReader = new BufferedReader(fReader);
+                FileWriter writer = new FileWriter(targetFile);
+            }
+        }
 
 
-        // Create a new graph controller
+
+
         // Generate graph - with optional args if no string - check if graph.txt exists
         // run railways system with returned graph data
         System.out.println("Done");
